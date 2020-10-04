7cb97aaf105d93f6caab455ff6e48d76a7b8e127
GoodGuyGregory <greg.witt625@gmail.com>
2020-10-04 00:19:51 -0400

    adds CreateObservable

--- rxjs-demos/src/examples/CreateObservable.vue
+++ rxjs-demos/src/examples/CreateObservable.vue
@@ -18,10 +18,61 @@
     </p>
 
     <h3>Code Example</h3>
+
+    <p>create a print method for your project's html</p>
+    <pre v-highlightjs><code class="javascript"> function print(val) {
+        let example = document.createElement('p');
+        example.innerText = val;
+        document.body.appendChild(example)
+    }</code></pre>
+    <p>create the observer to append data to the DOM</p>
+    <pre v-highlightjs><code class="javascript"> 
+         // creates an observable:
+         // the next method controls that the observer will emit, t
+         //his can be data or in this case a string
+         const observable = Rx.Observable.create( observer => {
+             observer.next( 'hello' )
+             observe.next( 'world' )
+         }
+
+         // in order to emit values from the observer. 
+         //objects wishing to use the data must subscribe 
+         //to the data stream emitted
+         observer.subscribe( val => print())
+
+         </code></pre>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import hljs from 'highlight.js'
+
+Vue.directive('highlightjs', {
+  deep: true,
+  bind: function (el, binding) {
+    // on first bind, highlight all targets
+    let targets = el.querySelectorAll('code')
+    targets.forEach((target) => {
+      // if a value is directly assigned to the directive, use this
+      // instead of the element content.
+      if (binding.value) {
+        target.textContent = binding.value
+      }
+      hljs.highlightBlock(target)
+    })
+  },
+  componentUpdated: function (el, binding) {
+    // after an update, re-fill the content and then highlight
+    let targets = el.querySelectorAll('code')
+    targets.forEach((target) => {
+      if (binding.value) {
+        target.textContent = binding.value
+        hljs.highlightBlock(target)
+      }
+    })
+  }
+});
 export default {
   name: "CreateObservable",
   components: {
@@ -56,6 +107,11 @@ h2 {
 li {
   padding: 5px;
   margin: 7px;
+}
+
+.javascript {
+  border-radius: 15px;
+  padding: 20px;
 }
 
 a {
